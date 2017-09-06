cfdd6b34c2baad10c3735bdafc44518afeb67010
danneggiasta <zlatanovic.nebojsa@yahoo.com>
2017-09-06 21:52:33 +0200

    styling added map

--- assets/js/main.js
+++ assets/js/main.js
@@ -79,3 +79,15 @@ $(function() {
 	});
 
 });
+
+function initMap() {
+  var uluru = {lat: 45.258, lng: 19.835};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
