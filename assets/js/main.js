f2167ced92edee4b10571edd9a1c111fec14590b
danneggiasta <zlatanovic.nebojsa@yahoo.com>
2017-09-06 20:44:36 +0200

    styling change of fonts

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,7 +35,7 @@ $('a[href*="#"]')
     }
   });
 
-  // When the user scrolls down 20px from the top of the document, show the button
+// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
